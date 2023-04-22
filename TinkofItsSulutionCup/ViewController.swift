aadbf09c98c6add40b152b8ac1ba050e16e951f7
Ruslan Lukmanov <lukmanov@vbankcenter.ru>
2023-04-22 12:15:46 +0300

    Initial Commit

---
+++ TinkofItsSulutionCup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkofItsSulutionCup
+//
+//  Created by Ruslan Lukmanov on 22.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
